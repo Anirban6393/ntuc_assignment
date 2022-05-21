8dc7b31b1bc167afb7c723ebee7da6798bd2d6a7
Anirban Kar Chaudhuri <anirban.karchaudhuri@gmail.com>
2022-05-21 18:38:13 +0800

    Update data_ingestion.py

--- transfomer/data_ingestion.py
+++ transfomer/data_ingestion.py
@@ -37,3 +37,16 @@ def execute_external_sql_script_file(script_file_path,db_name:str):
     file.close()
     # Execute the read out sql script string.
     execute_sql_script(sql_script_string,db_name)
+    
+    
+def ingest_data(dataframe,db_name, table):    
+    # Connect to sqlite3 database.
+    conn = sqlite3.connect(db_name)     
+    dataframe.to_sql(table, conn, if_exists='replace', index = False)
+    
+    
+    
+def query_data(query,db_name):    
+    # Connect to sqlite3 database.
+    conn = sqlite3.connect(db_name)    
+    pd.read_sql(query, conn)
