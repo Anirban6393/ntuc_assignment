9a7c55c299fd50527b0ed1281a7587e3c8e9b53e
Anirban Kar Chaudhuri <anirban.karchaudhuri@gmail.com>
2022-05-21 17:01:46 +0800

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -1,23 +1,7 @@
-import wget
 import os
-from zipfile import ZipFile
+from download import *
 
 os.chdir((os.getcwd()+'\\data'))
-
-def download_files(url:str):
-    link=url
-    wget.download(link)
-
-  
-def unzip_files(file:str):
-    # opening the zip file in READ mode
-    with ZipFile(file, 'r') as zip:
-        # printing all the contents of the zip file
-        zip.printdir()
-        # extracting all the files
-        print('Extracting all the files now...')
-        zip.extractall()
-        print('Downloaded {}!'.format(file))
 
 if __name__ == "__main__":
     #specify website name
