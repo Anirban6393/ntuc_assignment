eaa8eff853eb82423f40001a22d7d12303d1353e
Anirban Kar Chaudhuri <anirban.karchaudhuri@gmail.com>
2022-05-21 16:31:18 +0800

    Add files via upload

---
+++ sql/create_table.sql
@@ -0,0 +1,21 @@
+ 
+CREATE TABLE IF NOT EXISTS bank
+(
+age INT,
+job TEXT, 
+marital TEXT,
+education TEXT,
+default_status TEXT,    
+balance INT, 
+housing TEXT,
+loan TEXT,
+contact TEXT,
+day INT,
+month TEXT,
+duration INT,
+campaign INT,
+pdays INT,
+previous INT,
+poutcome TEXT,
+y TEXT        
+);
